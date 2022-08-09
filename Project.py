0241d721b471da05bce4fb78d8e98f7809b359c7
Zeptika <diefer411@gmail.com>
2022-08-09 14:31:45 -0500

    V3 Project Shady

--- Project.py
+++ Project.py
@@ -14,80 +14,79 @@ def printmat (matriz):
 def recomendacion(Matriz, dim):
 
     for i in range(dim[1]):
-        if i >= 0 and i <= 2:
-            #print('MACRONUTRIENTES PRIMARIOS \n')
+        
+        if i == 0:
+            #print('pH Del Suelo \n')
             print('\n'+str(Matriz[0][i]) + ':\nValor Optimo de cultivo: '
-            + str(Matriz[1][i]) + '\npH Muestra: '+ str(Matriz[2][i])
+            + str(Matriz[1][i]) + '\nMuestra: '+ str(Matriz[2][i])
             + '\nResultado: '+ str(Matriz[3][i]))
 
             if Matriz[3][i] > 0:
                 print('Recomendación 1: Incrementar pH del suelo en "resultado C20"')
-                print('Recomendación 2: Optimo')
             else:
                 print('Recomendación 1: disminuir pH del suelo en "resultado C20"')
+        if i > 0 and i <= 3:
+            #print('MACRONUTRIENTES PRIMARIOS \n')
+            print('\n'+str(Matriz[0][i]) + ':\nValor Optimo de cultivo: '
+            + str(Matriz[1][i]) + '\nMuestra: '+ str(Matriz[2][i])
+            + '\nResultado: '+ str(Matriz[3][i]))
+
+            if Matriz[3][i] > 0:
+                print('Recomendación 2: Optimo')
+            else:
                 print('Recomendación 2: Incrementar macronutrientes primarios en el suelo')
-        elif i > 2 and i <= 5:
+        elif i > 3 and i <= 6:
 
             #print('MACRONUTRIENTES SECUNDARIOS \n')
             print('\n'+str(Matriz[0][i]) + ':\nValor Optimo de cultivo: '
-            + str(Matriz[1][i]) + '\npH Muestra: '+ str(Matriz[2][i])
+            + str(Matriz[1][i]) + '\nMuestra: '+ str(Matriz[2][i])
             + '\nResultado: '+ str(Matriz[3][i]))
 
             if Matriz[3][i] > 0:
-                print('Recomendación 1: Incrementar pH del suelo en "resultado C20"')
-                print('Recomendación 2: Optimo')
+                print('Recomendación 3: Optimo')
             else:
-                print('Recomendación 1: disminuir pH del suelo en "resultado C20"')
-                print('Recomendación 2: Incrementar macronutrientes secundarios en el suelo')      
-        elif i > 5 and i <= 9:
+                print('Recomendación 3: Incrementar macronutrientes secundarios en el suelo')      
+        elif i > 6 and i <= 10:
 
             #print('MICRONUTRIENTES \n')
             print('\n'+str(Matriz[0][i]) + ':\nValor Optimo de cultivo: '
-            + str(Matriz[1][i]) + '\npH Muestra: '+ str(Matriz[2][i])
+            + str(Matriz[1][i]) + '\nMuestra: '+ str(Matriz[2][i])
             + '\nResultado: '+ str(Matriz[3][i]))
 
             if Matriz[3][i] > 0:
-                print('Recomendación 1: Incrementar pH del suelo en "resultado C20"')
-                print('Recomendación 2: Optimo')
+                print('Recomendación 4: Optimo')
             else:
-                print('Recomendación 1: disminuir pH del suelo en "resultado C20"')
-                print('Recomendación 2: Incrementar micronutrientes en el suelo')
-        elif i > 9 and i <= 13:
+                print('Recomendación 4: Incrementar micronutrientes en el suelo')
+        elif i > 10 and i <= 14:
 
-            #print('MICRONUTRIENTES \n')
+            #print('Metales pesados sin funcion biologica \n')
             print('\n'+str(Matriz[0][i]) + ':\nValor Optimo de cultivo: '
-            + str(Matriz[1][i]) + '\npH Muestra: '+ str(Matriz[2][i])
+            + str(Matriz[1][i]) + '\nMuestra: '+ str(Matriz[2][i])
             + '\nResultado: '+ str(Matriz[3][i]))
 
             if Matriz[3][i] > 0:
-                print('Recomendación 1: Incrementar pH del suelo en "resultado C20"')
-                print('Recomendación 2: Aprobado')
+                print('Recomendación 5: Aprobado')
             else:
-                print('Recomendación 1: disminuir pH del suelo en "resultado C20"')
-                print('Recomendación 2: Peligrosidad')
-        elif i > 13 and i <= 17:
+                print('Recomendación 5: Peligrosidad')
+        elif i > 14 and i <= 19:
 
-            #print('MICRONUTRIENTES \n')
+            #print('Oligoelementos \n')
             print('\n'+str(Matriz[0][i]) + ':\nValor Optimo de cultivo: '
-            + str(Matriz[1][i]) + '\npH Muestra: '+ str(Matriz[2][i])
+            + str(Matriz[1][i]) + '\nMuestra: '+ str(Matriz[2][i])
             + '\nResultado: '+ str(Matriz[3][i]))
 
             if Matriz[3][i] > 0:
-                print('Recomendación 1: Incrementar pH del suelo en "resultado C20"')
-                print('Recomendación 2: Optimo')
+                print('Recomendación 6: Optimo')
             else:
-                print('Recomendación 1: disminuir pH del suelo en "resultado C20"')
-                print('Recomendación 2: Peligrosidad')
-
-
+                print('Recomendación 6: Peligrosidad')
 
 
 def run():
        
-    l = [['N', 'P', 'K', 'Ca', 'Mg', 'S', 'Cu', 'Mn', 'Zn', 'Fe', 'Cr', 'Ni', 'Pb', 'Cd', 'B', 'Ba', 'Se', 'As'], 
-        [5.2, 2.1, 4.3, 0.5, 0.6, 0.5, 0.4, 0.4, 0.3, 0.3, 0.5, 0.3, 0.2, 0.2, 0.8, 0.9, 0.15, 0.2], 
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+    l = [['pH','N', 'P', 'K', 'Ca', 'Mg', 'S', 'Cu', 'Mn', 'Zn', 'Fe', 'Cr', 'Ni', 'Pb', 'Cd', 'B', 'Ba', 'Se', 'As'], 
+        [7, 5.2, 2.1, 4.3, 0.5, 0.6, 0.5, 0.4, 0.4, 0.3, 0.3, 0.5, 0.3, 0.2, 0.2, 0.8, 0.9, 0.15, 0.2], 
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
 
     dim = np.shape(l)
 
